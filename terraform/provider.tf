26e924ac9690ba8aae841723a581b5b4b91cc0f9
ashrujitpal <ashrujitpal1@gmail.com>
2022-08-12 21:05:38 +0530

    Initial Commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
+  region = "us-east-1"
   access_key = "AKIAWYV5RVX6Y47W5OMN"
   secret_key = "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"
-  region     = "us-east-1"
 }
