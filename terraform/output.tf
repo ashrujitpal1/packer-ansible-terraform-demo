9a34e019a29dd0e372ecc1901f60cbb6f9588877
ashrujitpal <ashrujitpal1@gmail.com>
2022-08-13 09:03:21 +0530

    Initial Commit

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,7 +5,3 @@ output "publicIp" {
 output "curl" {
     value = "curl http://${aws_instance.ec2.public_ip}"
 }
-
-output "Login-with-Key" {
-    value = "ssh -i ${aws_key_pair.sshkey.key_name} ubuntu@${aws_instance.ec2.public_ip}"
-}
